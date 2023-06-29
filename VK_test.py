1835402827801aa917b6f2e32606dfd9ec9f107e
Azoxe <semenuchkin@yandex.ru>
2023-06-29 18:00:12 +0300

    token

--- VK_test.py
+++ VK_test.py
@@ -1,12 +1,12 @@
 import vk_api
+from secrets_1 import token
 from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
 def write_message (sender, message):
     authorize.method('messages.send',{'user_id':sender, 'message': message, 'random_id':get_random_id(), 'attachment':','.join(attachments)})
-    
-token = "vk1.a.nckBYzoLP7eiPIIlCS0NOERp-qNlojfNGbkPUUxbVtTbq3DMtsXGOnuvHhVZT028Xn9-RWpD40yvhGMhrUqVO4lB6reqGIZBg1jK12iU8PbMpHlX-kCtEo6_rXAhrzAxhR-zZb6GoSmO7ZNISt7T5X65ZUzdqRc1NJynQWzdgUG7NNJlDV2sBhM3Ox5SUUlLHHQth9e2L-5g8kK6mUgolg"
+
 image = "test_image.png"
 authorize = vk_api.VkApi(token = token)
 longpoll = VkLongPoll(authorize)
