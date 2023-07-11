badfa57bef675276040c0aefd8a17dbce9dfd904
Azoxe <semenuchkin@yandex.ru>
2023-07-11 16:46:16 +0300

    DB2

--- src/db/_sqlalchemy/models.py
+++ src/db/_sqlalchemy/models.py
@@ -10,11 +10,10 @@ class User(Base):
     __tablename__ = "User"
     
     user_id: Mapped[int] = mapped_column(primary_key=True)
-    gender = sq.Column(sq.String(length=6), unique = True)
+    gender = sq.Column(sq.String(length=15), unique = True)
     age = sq.Column(sq.Integer, nullable=False)
     city = sq.Column(sq.String(length=30), unique = True)
     
-    seen: Mapped['Seen'] = relationship(back_populates='User')
     favorites: Mapped['Favorites'] = relationship(back_populates='User')  
     blacklist: Mapped['Blacklist'] = relationship(back_populates='User')      
         
@@ -24,27 +23,13 @@ class Target(Base):
     target_id: Mapped[int] = mapped_column(primary_key=True)
     link = sq.Column(sq.String(length=100), unique = True)
     
-    seen: Mapped['Seen'] = relationship(back_populates='Target')
     favorites: Mapped['Favorites'] = relationship(back_populates='Target')  
     blacklist: Mapped['Blacklist'] = relationship(back_populates='Target')         
         
-class Seen(Base):
-    __tablename__ = "Seen"
-    
-    # id: Mapped[int] = mapped_column(primary_key=True)
-    
-    user_id: Mapped[int] = mapped_column(ForeignKey('User.user_id'))
-    target_id: Mapped[int] = mapped_column(ForeignKey('Target.target_id'), primary_key=True)
-
-    user: Mapped['User'] = relationship(back_populates='seen')
-    target: Mapped['Target'] = relationship(back_populates='seen')
-
     
 class Favorites(Base):
     __tablename__ = "Favorites"
-    
-    # id = sq.Column(sq.Integer, primary_key=True)
-    
+
     user_id: Mapped[int] = mapped_column(ForeignKey('User.user_id'))
     target_id: Mapped[int] = mapped_column(ForeignKey('Target.target_id'), primary_key=True)
     
